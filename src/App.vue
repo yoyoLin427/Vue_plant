3cd3239e5b78c913bf3fc4465c0a6db334dae29e
yoyoLin427 <F74064046@gs.ncku.edu.tw>
2021-05-11 15:21:55 +0800

    plant animation

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">     
+    <!-- 渲染 route 的位置 --> 
+    <router-view></router-view> 
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,41 @@
+// main.js是整個專案的入口處
+
+// 載入相關的套件、專案檔案等等。
+import Vue from 'vue';
+import Router from 'vue-router';
+import App from '@/App.vue';
+
+
+import Feeling from '@/views/Feeling.vue'
+import About from '@/views/About.vue'
+import Temp from '@/views/Temp.vue'
+import DiaryMain from '@/views/DiaryMain.vue'
+import DiaryWrite from '@/views/DiaryWrite.vue'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+/* 動畫 */
+import animated from 'animate.css' 
+Vue.use(animated)
+
+Vue.use(Router)
+Vue.use(BootstrapVue)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    { path: '/temp', component: Temp },
+    { path: '/feeling', component: Feeling },
+    { path: '/about', component: About },
+    { path: '/diary', component: DiaryMain },
+    { path: '/diary/write', component: DiaryWrite }
+  ]
+});
+// 初始化應用程序，使用 render 函式將 App 元件渲染出來
+// 然後將應用程序綁定在 DOM 上面，該 DOM 的 ID 是 #app 
+new Vue({
+  router: router,
+  render: h => h(App)
+}).$mount('#app')
